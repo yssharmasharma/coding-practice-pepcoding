cbe3b9df0c6b86ac97d00b3c6110def3c5b66b06
YASH SHARMA <42815063+yssharmasharma@users.noreply.github.com>
2023-01-07 14:47:23 +0530

    Update pattern6.java

--- pattern6.java
+++ pattern6.java
@@ -1,1 +1,42 @@
+import java.util.*;
 
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        // write ur code here
+        
+        int n=scn.nextInt();
+        int spaces=1;
+        int star=n/2+1;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=star;j++){
+                System.out.print("*\t");
+            }
+            for(int k=1;k<=spaces;k++){
+            System.out.print("\t");
+            
+            }
+            
+            for(int j=1;j<=star;j++){
+                System.out.print("*\t");
+            }
+            if(i<=n/2){
+                spaces+=2;
+                star--;
+                
+            }
+            else {
+                spaces-=2;
+                star++;
+            }
+            System.out.println();
+            
+            
+            
+            
+        }
+
+    }
+}
