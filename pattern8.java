09d812b5559cd5606da6cfd5bdedee8985755633
YASH SHARMA <42815063+yssharmasharma@users.noreply.github.com>
2023-01-07 15:25:02 +0530

    Update pattern8.java

--- pattern8.java
+++ pattern8.java
@@ -1,1 +1,27 @@
+import java.util.*;
 
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        // write ur code here
+        int n=scn.nextInt();
+        int step=n;
+        for(int i=n;i>=1;i--){
+            for(int j=1;j<=n;j++){
+                if(j==step){
+                    System.out.print("*\t");
+                }
+                else{
+                    System.out.print("\t");
+                }
+                
+            }
+            step--;
+            System.out.println();
+           
+        }
+
+    }
+}
