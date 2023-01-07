e3d553a7f7ebd4db1a5f75661c7a1ea21d15106a
YASH SHARMA <42815063+yssharmasharma@users.noreply.github.com>
2023-01-07 15:24:32 +0530

    Update pattern7.java

--- pattern7.java
+++ pattern7.java
@@ -1,1 +1,29 @@
 
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        // write ur code here
+        int n=scn.nextInt();
+        int star=1;
+        int space=0;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                if(i==j){
+                    System.out.print("*\t");
+                }
+                else{
+                    System.out.print("\t");
+                }
+                
+               
+            }
+            System.out.println();
+            
+        }
+
+    }
+}
