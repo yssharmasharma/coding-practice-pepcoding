731e02e82d620896723a2d53e2c5b2abff777bb8
YASH SHARMA <42815063+yssharmasharma@users.noreply.github.com>
2023-01-08 22:27:15 +0530

    Update pattern14.java

--- pattern14.java
+++ pattern14.java
@@ -1,1 +1,19 @@
 
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        // write ur code here
+        int n=scn.nextInt();
+        int prod=1;
+        for(int i=1;i<=10;i++){
+            prod=n*i;
+            System.out.println(n+" * "+i+" = "+prod);
+            
+        }
+
+    }
+}
