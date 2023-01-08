ccc6e1cf40812ffdf58c250dbf6b4b68da86742b
YASH SHARMA <42815063+yssharmasharma@users.noreply.github.com>
2023-01-08 14:29:47 +0530

    Update pattern11.java

--- pattern11.java
+++ pattern11.java
@@ -1,1 +1,26 @@
 
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        // write ur code here
+        int n= scn.nextInt();
+        int count=1;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                if(i>j){
+                    System.out.print(count+"\t");
+                    count++;
+                }
+                else{
+                    System.out.println();
+                }
+            }
+            
+        }
+
+    }
+}
