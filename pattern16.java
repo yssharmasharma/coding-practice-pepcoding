5a14103d2fc1b244ba3d5b807ab5720d78e952e8
YASH SHARMA <42815063+yssharmasharma@users.noreply.github.com>
2023-01-09 23:05:49 +0530

    Update pattern16.java

--- pattern16.java
+++ pattern16.java
@@ -1,1 +1,31 @@
 
+import java.util.*;
+
+public class Main{
+
+public static void main(String[] args) {
+    Scanner scn = new Scanner(System.in);
+
+     // write ur code here
+     int n=scn.nextInt();
+     for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= n; j++){
+            if(j<= i){
+            System.out.print(j+"\t");
+            }
+            else{ 
+                    System.out.print("\t");
+            }
+        }
+        for(int j = n - 1; j >= 1; j--){
+            if(j <= i){ System.out.print(j+"\t");}
+            else{ 
+            System.out.print("\t");
+            }
+        }
+        System.out.println();
+        
+     }
+
+ }
+}
