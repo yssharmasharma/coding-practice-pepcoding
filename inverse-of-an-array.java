523ce001566f75440ab038ff5195da74a7a91a3f
YASH SHARMA <42815063+yssharmasharma@users.noreply.github.com>
2023-01-15 12:52:45 +0530

    Create inverse-of-an-array.java

---
+++ inverse-of-an-array.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+  public static void display(int[] a){
+    StringBuilder sb = new StringBuilder();
+
+    for(int val: a){
+      sb.append(val + "\n");
+    }
+    System.out.println(sb);
+  }
+
+  public static int[] inverse(int[] a){
+    // write your code here
+    int n=a.length;
+    int [] arr=new int[n];
+    for(int i=0;i<n;i++){
+        int j=a[i];
+        arr[j]=i;
+        
+        
+    }
+    return arr;
+  }
+
+public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    int n = Integer.parseInt(br.readLine());
+    int[] a = new int[n];
+    for(int i = 0; i < n; i++){
+       a[i] = Integer.parseInt(br.readLine());
+    }
+
+    int[] inv = inverse(a);
+    display(inv);
+ }
+
+}
