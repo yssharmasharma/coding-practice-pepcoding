9227483fb3cdf24b84b56540deee89446acdd6a1
YASH SHARMA <42815063+yssharmasharma@users.noreply.github.com>
2023-01-25 10:53:33 +0530

    Create Saddle Price.java

---
+++ Saddle Price.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        // write your code here
+        Scanner s= new Scanner(System.in);
+        int n=s.nextInt();
+        int[][]a=new int[n][n];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                a[i][j]=s.nextInt();
+            }
+                
+        }
+       for(int i=0;i<n;i++){
+           int svj=0;
+           for(int j=0;j<n;j++){
+               if(a[i][j]<a[i][svj]){
+                   svj=j;
+               }
+           }
+           boolean flag=true;
+           for(int k=0;k<n;k++){
+               if(a[k][svj]>a[i][svj]){
+                   flag=false;
+                   break;
+               }
+           }
+           if(flag==true){
+               System.out.println(a[i][svj]);
+               return;
+           }
+       }
+       System.out.println("Invalid input");
+       
+        
+        
+        
+    }
+
+}
