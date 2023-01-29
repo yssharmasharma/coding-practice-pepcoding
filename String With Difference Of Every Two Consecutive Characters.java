23a68a5483a0611e888f14b6c337d8d007ebe6df
YASH SHARMA <42815063+yssharmasharma@users.noreply.github.com>
2023-01-29 19:47:55 +0530

    Create String With Difference Of Every Two Consecutive Characters.java

---
+++ String With Difference Of Every Two Consecutive Characters.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static String solution(String str){
+		// write your code here
+		StringBuilder ans=new StringBuilder();
+		for(int i=0;i<str.length();i++){
+		    if(i+1<str.length()){
+		        int diff=str.charAt(i+1)-str.charAt(i);
+		        ans.append(str.charAt(i));
+		        ans.append(diff);
+		    }
+		    else{
+		        ans.append(str.charAt(i));
+		    }
+		    
+		}
+
+		return ans.toString();
+	}
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		String str = scn.next();
+		System.out.println(solution(str));
+	}
+
+}
