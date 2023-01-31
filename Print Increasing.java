5127cf593e70f2b1c9328bdafee772dec6601a95
YASH SHARMA <42815063+yssharmasharma@users.noreply.github.com>
2023-01-31 19:29:38 +0530

    Create Print Increasing.java

---
+++ Print Increasing.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        // write your code here
+        Scanner s= new Scanner(System.in);
+        int n=s.nextInt();
+        printIncreasing(n);
+    }
+
+    public static void printIncreasing(int n){
+        if(n==0){
+            return;
+        }
+        printIncreasing(n-1);
+        System.out.println(n);
+    }
+
+}
