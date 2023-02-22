17f8232d5511913c780fa8d4b051aeaa38f8c15b
YASH SHARMA <42815063+yssharmasharma@users.noreply.github.com>
2023-02-22 11:39:45 +0530

    Create Target Sum Subsets_rec.java

---
+++ Target Sum Subsets_rec.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        int []a=new int[n];
+        for(int i=0;i<n;i++){
+            a[i]=s.nextInt();
+        }
+        int tar=s.nextInt();
+        printTargetSumSubsets(a,0,"",0,tar);
+
+    }
+
+    // set is the subset
+    // sos is sum of subset
+    // tar is target
+    public static void printTargetSumSubsets(int[] arr, int idx, String set, int sos, int tar) {
+        
+         if(sos > tar){
+            return;
+        }
+        if(idx==arr.length){
+            if(sos==tar){
+                System.out.println(set+".");
+            }
+            return;
+        }
+        printTargetSumSubsets(arr, idx + 1, set + arr[idx]+ ", " , sos + arr[idx], tar);
+        printTargetSumSubsets(arr, idx + 1, set, sos, tar);
+
+    }
+
+}
