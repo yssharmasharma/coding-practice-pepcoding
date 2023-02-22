05c1d9a91fa2de8f73ace07b34fb7409950c247e
YASH SHARMA <42815063+yssharmasharma@users.noreply.github.com>
2023-02-22 11:19:47 +0530

    Create Flood Fill_rec.java

---
+++ Flood Fill_rec.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int m = scn.nextInt();
+        int[][] arr = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                arr[i][j] = scn.nextInt();
+            }
+        }
+        floodfill(arr, 0, 0, "");
+    }
+    
+    // asf -> answer so far
+    public static void floodfill(int[][] maze, int sr, int sc, String asf) {
+        if(sr < 0 || sc < 0 || sr == maze.length || sc == maze[0].length || maze[sr][sc] == 1){
+            return;
+        }
+        if(sr == maze.length-1 && sc == maze[0].length-1){
+            System.out.println(asf);
+            return;
+        }
+        floodfill(maze,sr - 1,sc,asf + "t");
+        floodfill(maze,sr,sc - 1,asf + "l");
+        floodfill(maze,sr + 1,sc,asf + "d");
+        floodfill(maze,sr,sc + 1,asf + "r");
+        
+    
+    }
+}
