b708e1ad42917e9bcb51c05f37049e3f0e8d6c16
YASH SHARMA <42815063+yssharmasharma@users.noreply.github.com>
2023-03-19 22:32:25 +0530

    Create Stack To Queue Adapter - Add Efficient.java

---
+++ Stack To Queue Adapter - Add Efficient.java
@@ -0,0 +1,77 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+  public static class StackToQueueAdapter {
+    Stack<Integer> mainS;
+    Stack<Integer> helperS;
+
+    public StackToQueueAdapter() {
+      mainS = new Stack<>();
+      helperS = new Stack<>();
+    }
+
+    int size() {
+      // write your code here
+      return mainS.size();
+    }
+
+    void add(int val) {
+      // write your code here
+      return mainS.push(val);
+    }
+
+    int remove() {
+      // write your code here
+      while(mainS.size()>1){
+          helperS.push(mainS.pop())
+      }
+      int val=mainS.pop();
+      while(helperS.size()>0){
+          mainS.push(helpers.pop());
+      }
+      return val;
+      
+    }
+
+    int peek() {
+      // write your code here
+      while(mainS.size()>1){
+          helperS.push(mainS.pop())
+      }
+      int val=mainS.pop();
+      helperS.push(val)
+      while(helperS.size()>0){
+          mainS.push(helpers.pop());
+      }
+      return val;
+    }
+  }
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StackToQueueAdapter qu = new StackToQueueAdapter();
+
+    String str = br.readLine();
+    while (str.equals("quit") == false) {
+      if (str.startsWith("add")) {
+        int val = Integer.parseInt(str.split(" ")[1]);
+        qu.add(val);
+      } else if (str.startsWith("remove")) {
+        int val = qu.remove();
+        if (val != -1) {
+          System.out.println(val);
+        }
+      } else if (str.startsWith("peek")) {
+        int val = qu.peek();
+        if (val != -1) {
+          System.out.println(val);
+        }
+      } else if (str.startsWith("size")) {
+        System.out.println(qu.size());
+      }
+      str = br.readLine();
+    }
+  }
+}
